78c1858a9f0548365c10d0289df1f7a67132c0a3
ryanseptiyanto <nur.septiyanto@gmail.com>
2025-05-07 12:38:29 +0700

    feat(pos): pindahkan tombol Clear dan Bayar ke footer halaman kasir

--- src/views/Pos.vue
+++ src/views/Pos.vue
@@ -109,12 +109,6 @@
             </div>
           </div>
   
-          <!-- Tombol Bayar & Clear -->
-          <div class="d-flex justify-content-end mb-4">
-            <button class="btn btn-secondary me-2" @click="clearCart()">Clear</button>
-            <button class="btn btn-success" @click="openPaymentModal()" :disabled="cart.length===0">Bayar</button>
-          </div>
-  
           <!-- Modal Pembayaran -->
           <div class="modal fade" id="paymentModal" tabindex="-1" ref="paymentModalEl">
             <div class="modal-dialog">
@@ -145,7 +139,16 @@
             </div>
           </div>
         </div>
-      </div>
+        
+        <!-- Footer untuk Tombol -->
+        <footer class="footer bg-white border-top py-3">
+        <div class="container-xl d-flex justify-content-end">
+            <button class="btn btn-secondary me-2" @click="clearCart()">Clear</button>
+            <button class="btn btn-success" @click="openPaymentModal()" :disabled="cart.length===0">Bayar</button>
+        </div>
+        </footer>
+        </div>
+        
     </div>
   </template>
   
